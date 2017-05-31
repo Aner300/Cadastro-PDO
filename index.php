e883e98b4c2732480768d82ba2152c1ddcb23b56
Anércio Ribeiro <anercio.ribeiro@tistech.co.ao>
2017-05-31 15:42:13 +0100

    Commit Inicial

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php ini_set('dispaly_errors',1); ?>
+<?php
+echo"Cadastro-PDO";
+
+?>
+<!DOCTYPE html>
+<html lang="pt-PT">
+  <head>
+    <meta charset="utf-8">
+    <title>Cadastro PDO</title>
+  </head>
+  <body>
+
+  </body>
+</html>
